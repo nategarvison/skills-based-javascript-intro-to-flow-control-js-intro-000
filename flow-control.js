4afcd9e322b834cdd460c4ce55ae474d2eab74b7
nategarvison <ngarvison@gmail.com>
2017-08-21 23:55:42 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -14,7 +14,13 @@ function teenager(age) {
 }
 
 function ageChecker(age) {
-
+  if (age >= 13 && age <= 19) {
+    return 'You are a teenager!';
+  } else if (age < 13) {
+    return 'You are a kid';
+  } else if (age > 19) {
+    return 'You are a grownup';
+  }
 }
 
 function ternaryTeenager(age) {
@@ -24,5 +30,3 @@ function ternaryTeenager(age) {
 function switchAge(age) {
 
 }
-
-basicTeenager(15);
